d4b7e4a58e1bcc27e8cd35f0dbc2356a0f0d0ab1
konverner <konst.verner@gmail.com>
2024-10-19 12:16:34 +0200

    feat: add feadback support

--- src/telegram_ai_tutor/main.py
+++ src/telegram_ai_tutor/main.py
@@ -1,6 +1,7 @@
 from telegram_ai_tutor.api.bot import start_bot
-from telegram_ai_tutor.db.database import create_tables
+from telegram_ai_tutor.db.database import create_tables, drop_tables
 
 if __name__ == "__main__":
+    drop_tables()
     create_tables()
     start_bot()
